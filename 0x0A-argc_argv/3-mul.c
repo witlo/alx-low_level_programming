fe6e910d752f86e75c58f6f269853f3681c2fb19
witlo <danielmacharia333@gmail.com>
2023-10-24 19:12:53 -0700

    Write a program that multiplies two numbers.
    Your program should print the result of the multiplication, followed by a new line
    You can assume that the two numbers and result of the multiplication can be stored in an integer
    If the program does not receive two arguments, your program should print Error, followed by a new line, and return 1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,101 +1,23 @@
-#include "main.h"
-
-int string_to_integer(char *str);
-int product_of_two_numbers(int number1, int number2);
-void display_number(int number);
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* main - Starting point of the program
-*
-* @argc: Count of command line arguments
-*
-* @argv: Array containing the command line arguments
-*
-* Return: 0, indicating successful execution
-*/
+ * main - calculates product of 2 numbers passed as command line arguments
+ * @argc: count of command line arguments
+ * @argv: array of command line arguments
+ * Return: 1 for incorrect number of arguments, 0 for successful execution
+ */
 
 int main(int argc, char *argv[])
 {
-	if (argc == 3)
-	{
-		display_number(product_of_two_numbers(string_to_integer(argv[1]), string_to_integer(argv[2])));
-		_putchar('\n');
-		return (0);
-	}
-	else
-	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		return (1);
-	}
-}
-
-/**
-* string_to_integer - Transforms a string to an integer
-*
-* @str: String to be transformed
-*
-* Return: The integer equivalent of the string
-*/
-
-int string_to_integer(char *str)
-{
-	int sign = 1;
-	int total = 0;
-
-	while (*str)
-	{
-		if (*str == '-')
-			sign *= -1;
-		else if (*str >= '0' && *str <= '9')
-			total = (total * 10) + (*str - '0');
-		else if (total)
-			break;
-
-		str++;
-	}
-
-	return (total * sign);
-}
-
-/**
-* product_of_two_numbers - Calculates the product of two integers
-*
-* @number1: First integer
-*
-* @number2: Second integer
-*
-* Return: Product of number1 and number2
-*/
-
-int product_of_two_numbers(int number1, int number2)
-{
-	return (number1 * number2);
-}
-
-/**
-* display_number - Displays an integer to the standard output
-*
-* @number: Integer to be displayed
-*
-* Return: None
-*/
-
-void display_number(int number)
-{
-	unsigned int absolute_number;
-
-	absolute_number = (number < 0) ? -number : number;
-
-	if (number < 0)
-		_putchar('-');
-
-	if (absolute_number / 10)
-		display_number(absolute_number / 10);
-
-	_putchar((absolute_number % 10) + '0');
+    if (argc == 3)
+    {
+        printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+        return (0);
+    }
+    else
+    {
+        printf("Error\n");
+        return (1);
+    }
 }
