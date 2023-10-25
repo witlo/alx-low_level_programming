5d525da050d6d1d4f753bc18ace060b30950b40e
witlo <danielmacharia333@gmail.com>
2023-10-24 19:43:09 -0700

    Write a program that multiplies two numbers.
    Your program should print the result of the multiplication, followed by a new line
    You can assume that the two numbers and result of the multiplication can be stored in an integer
    If the program does not receive two arguments, your program should print Error, followed by a new line, and return 1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,103 +1,23 @@
-#include "main.h"
-
-int str_to_int(char *str);
-int product_of_two(int num1, int num2);
-void display_num(int num);
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* main - Initial point
-*
-* @num_args: Count of arguments in @arg_vals
-*
-* @arg_vals: String array of arguments from command line
-*
-* Return: 0 if success, 1 if fail
-*/
+ * main - calculates product of 2 numbers passed as command line arguments
+ * @argc: count of command line arguments
+ * @argv: array of command line arguments
+ * Return: 1 for incorrect number of arguments, 0 for successful execution
+ */
 
-int main(int num_args, char *arg_vals[])
+int main(int argc, char *argv[])
 {
-	if (num_args == 3)
+	if (argc == 3)
 	{
-		display_num(product_of_two(str_to_int(arg_vals[1]), str_to_int(arg_vals[2])));
-		_putchar('\n');
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 		return (0);
 	}
 	else
 	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
+		printf("Error\n");
 		return (1);
 	}
-}
-
-/**
-* str_to_int - Transforms a string to an integer
-*
-* @str: String to be transformed
-*
-* Return: Integer obtained or (0) if none
-*/
-
-int str_to_int(char *str)
-{
-	int sign_mult = 1;
-	int result = 0;
-
-	while (*str)
-	{
-		if (*str == '-')
-			sign_mult *= -1;
-		else if (*str >= '0' && *str <= '9')
-			result = result * 10 + (*str - '0');
-		else if (result)
-			break;
-		str++;
-	}
-
-	return (result * sign_mult);
-}
-
-/**
-* product_of_two - Calculates the product of two integers
-*
-* @num1: First integer
-*
-* @num2: Second integer
-*
-* Return: Product of num1 and num2
-*/
-
-int product_of_two(int num1, int num2)
-{
-	return (num1 * num2);
-}
-
-/**
-* display_num - Outputs an integer as characters
-*
-* @num: Integer to be printed
-*
-* Return: void
-*/
-
-void display_num(int num)
-{
-	unsigned int num_abs;
-
-	num_abs = num;
-
-	if (num < 0)
-	{
-		_putchar('-');
-		num_abs = -num;
-	}
-
-	if (num_abs / 10 != 0)
-		display_num(num_abs / 10);
-
-	_putchar((num_abs % 10) + '0');
 }
